bed7a3b90f1b45ad985864fcd67c3e85a0ba8e89
구룡 <julong@gulyong-ui-MacBook-Pro.local>
2018-04-25 11:49:04 +0900

    블로그,세션,몽고디비 초기화

---
+++ blog-session-mongodb/db/postModel.js
@@ -0,0 +1,9 @@
+module.exports = {
+    post: {
+      name: { type: String, required: true },
+      date:{type: String, required: true},
+      title: {type: String, required: true},
+      content: {type: String, required: true}
+    }
+  };
+  
