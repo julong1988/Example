5416265b40954ccc169f0f9ff721dfec1d1bf4c3
구룡 <julong@gulyong-ui-MacBook-Pro.local>
2018-05-17 13:01:00 +0900

    1차 get post put delete

---
+++ rest/db/model.js
@@ -0,0 +1,9 @@
+module.exports = {
+    user: {
+      fname: { type: String, required: true },
+      lname: { type: String, required: true },
+      email: { type: String, required: true },
+      tel: { type: String, required: true },
+      password: { type: String, required: true },
+    }
+  };
